7cae39d473e4888dbe236e4326cb015dc75b237d
kimikazu furukawa <g.guquan@gmail.com>
2016-07-02 05:32:24 +0000

    Add password resets & email configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'serene-wave-81247.herokuapp.com.herokuapp.com'
+  host = 'serene-wave-81247.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
